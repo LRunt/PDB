1628b518f36b1dc760db5dd10906479b82031534
Lukas Runt <lukas.runt@seznam.cz>
2023-12-03 18:27:34 +0100

    connection with database

--- Semestral work/main.py
+++ Semestral work/main.py
@@ -1,4 +1,30 @@
 import psycopg2
+from config import config
+
+
+def connection():
+    try:
+        params = config()
+        print("Connecting to the postgreSQL database ...")
+        connect = psycopg2.connect(**params)
+
+        cursor = connect.cursor()
+        print("PostgreSQL database version: ")
+        cursor.execute("SELECT version()")
+        db_version = cursor.fetchall()
+        print(db_version)
+    except(Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if connect is not None:
+            connect.close()
+
 
 if __name__ == "__main__":
-    connection = psycopg2.connect(host="localhost", port="5432", database="hehe", user="postgres", password="kolokolo")
+    connection = psycopg2.connect(host="localhost", port="5432", database="test", user="postgres", password="kolokolo")
+    cursor = connection.cursor()
+    print("PostgreSQL database version: ")
+    cursor.execute("SELECT version()")
+    db_version = cursor.fetchall()
+    print(db_version)
+    connection.close()
