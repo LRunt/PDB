a126c4fb0b49f4b92de61604d794c75804f9b17a
Lukas Runt <lukas.runt@seznam.cz>
2023-12-12 19:15:55 +0100

    cleaning workspace

--- Semestral work/laz_to_tin.py
+++ Semestral work/laz_to_tin.py
@@ -1,19 +1,7 @@
 import laspy
-import pylas
 
-import numpy as np
-import pylas as pylas
-from scipy.spatial import Delaunay
+
 
 if __name__ == "__main__":
-    with pylas.open('data/AS01/AS01.laz') as fh:
-        print('Points from Header:', fh.header.point_count)
-        las = fh.read()
-        print(las)
-        print('Points from data:', len(las.points))
-        ground_pts = las.classification == 2
-        bins, counts = np.unique(las.return_number[ground_pts], return_counts=True)
-        print('Ground Point Return Number distribution:')
-        for r,c in zip(bins,counts):
-            print('    {}:{}'.format(r,c))
+    las = laspy.read("data/AS01/AS01.laz")
 
--- Semestral work/main.py
+++ Semestral work/main.py
@@ -21,10 +21,24 @@ def connection():
 
 
 if __name__ == "__main__":
-    connection = psycopg2.connect(host="localhost", port="5432", database="test", user="postgres", password="kolokolo")
-    cursor = connection.cursor()
-    print("PostgreSQL database version: ")
-    cursor.execute("SELECT version()")
-    db_version = cursor.fetchall()
-    print(db_version)
-    connection.close()
+    try:
+        connection = psycopg2.connect(host="localhost", port="5432", database="test", user="postgres", password="kolokolo")
+        cursor = connection.cursor()
+        print("PostgreSQL database version: ")
+        cursor.execute("SELECT version()")
+        db_version = cursor.fetchall()
+        # Create a table
+        create_table_query = '''CREATE TABLE employee(
+                                               ID SERIAL PRIMARY KEY,
+                                               NAME TEXT NOT NULL,
+                                               AGE INT NOT NULL,
+                                         l()
+        print(db_version)      ADDRESS CHAR(50));
+                                       '''
+        cursor.execute(create_table_query)
+        connection.commit()
+        print("Table created successfully")
+        cursor.close()
+        connection.close()
+    except (Exception, psycopg2.Error) as error:
+        print("Error while creating a table", error)
