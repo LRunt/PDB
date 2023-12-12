4699b80d1176f7d0678d4a5ee8a451cbd1675146
Lukas Runt <lukas.runt@seznam.cz>
2023-12-12 19:44:58 +0100

    Showing triangular data

--- Semestral work/main.py
+++ Semestral work/main.py
@@ -1,5 +1,11 @@
 import psycopg2
 from config import config
+import geopandas as gpd
+from sqlalchemy import create_engine
+import matplotlib
+matplotlib.use("tkagg")
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def connection():
@@ -20,25 +26,48 @@ def connection():
             connect.close()
 
 
-if __name__ == "__main__":
+def get_geometry_from_database():
     try:
         connection = psycopg2.connect(host="localhost", port="5432", database="test", user="postgres", password="kolokolo")
         cursor = connection.cursor()
-        print("PostgreSQL database version: ")
-        cursor.execute("SELECT version()")
-        db_version = cursor.fetchall()
-        # Create a table
-        create_table_query = '''CREATE TABLE employee(
-                                               ID SERIAL PRIMARY KEY,
-                                               NAME TEXT NOT NULL,
-                                               AGE INT NOT NULL,
-                                         l()
-        print(db_version)      ADDRESS CHAR(50));
-                                       '''
-        cursor.execute(create_table_query)
-        connection.commit()
-        print("Table created successfully")
+        print("Triangular data: ")
+        query = "SELECT geom FROM public.benesov_exported ORDER BY gid ASC"
+        cursor.execute(query)
+        triangles = cursor.fetchall()
+        print(triangles)
         cursor.close()
         connection.close()
     except (Exception, psycopg2.Error) as error:
         print("Error while creating a table", error)
+
+
+def show_plot(gdf):
+    fig, ax = plt.subplots()
+    gdf.plot(ax=ax, facecolor='none', edgecolor='red')
+
+    # Customize the plot as needed
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_title('Triangle Visualization')
+
+    plt.show()
+
+if __name__ == "__main__":
+    engine = create_engine('postgresql://postgres:kolokolo@localhost:5432/test')
+    query = "SELECT * FROM benesov_exported;"
+    gdf = gpd.read_postgis(query, engine, geom_col='geom')
+
+    # Extract the x, y coordinates from the geometry
+    gdf['x'] = gdf['geom'].apply(lambda geometry: geometry.centroid.x)
+    gdf['y'] = gdf['geom'].apply(lambda geometry: geometry.centroid.y)
+
+    # Plot the 2D terrain
+    fig, ax = plt.subplots()
+    gdf.plot(ax=ax, facecolor='none', edgecolor='red', marker='o', markersize=5)
+
+    # Customize the plot as needed
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_title('2D Terrain Model Visualization')
+
+    plt.show()
